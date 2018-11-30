8f26f87a6c21c7e86d31da3ae4b491309ba1a0e9
Andrew Chinery <andrewchinery@gmail.com>
2018-11-30 15:24:17 +0000

    initial commit

---
+++ src/main/java/HasBath.java
@@ -0,0 +1,31 @@
+
+
+public class HasBath extends Problem{
+	
+	public HasBath(){
+		super(1, 3, "HASBATH");
+		throw new RuntimeException();
+	}
+
+	@Override
+	protected String getQuestion() {
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < rng.nextInt(4); i++) {
+			sb.append('X');
+		}
+		sb.append("BATH");
+		for(int i = 0; i < rng.nextInt(4); i++) {
+			sb.append('X');
+		}
+		return sb.toString();
+	}
+
+	@Override
+	protected String getAnswer(String question) {
+		if(question.matches(".?BATH.*")) {
+			return "TRUE";
+		} else {
+			return "FALSE";
+		}
+	}
+}
